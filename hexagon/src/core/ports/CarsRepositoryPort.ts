9118e8b6cb282cd9f942de481a4027eb9c61f516
abdelghani hamaz <abdelghani.hamaz.pro@gmail.com>
2024-05-15 16:53:40 +0200

    clear all repositories

--- hexagon/src/core/ports/CarsRepositoryPort.ts
+++
@@ -1,9 +0,0 @@
-import { Cars } from "../entity/Cars";
-
-export interface CarsRepositoryPort {
-  findAll(): Promise<Cars[]>;
-  findByOne(param: string): Promise<Cars | null>;
-  saveCar(user: Cars, id: string): Promise<Cars>;
-  deleteCar(id: string): Promise<boolean>;
-  updateCar(id: string, user: Cars): Promise<Cars>;
-}
